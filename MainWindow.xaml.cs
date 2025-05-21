265d56b5c6035707a809a81ce467f56d5bbc1eab
Ndiphiwe <Ndiphiwecaeser@gmail.com>
2025-05-22 00:56:29 +0200

    repo name displays in the tab

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,12 +12,15 @@ namespace WindowsPlatform
 		string? fetch;
 
 		private object? ButtonSender;
-		string? Name;
+		private string? currentRepoName;
 		public MainWindow()
 		{
 			InitializeComponent();
+
 			MenuHandler handler = new MenuHandler();
 
+			currentRepoName = GetCurrentRepositoryName();
+			firstTab.Header = currentRepoName;
 			handler.InitializeMenuButtons(btn_file, btn_view, btn_help, btn_tools);
 		}
 		/// TODO :: Initialize a repo then add to repos 
@@ -26,33 +29,50 @@ namespace WindowsPlatform
 			//Clean Up
 			var repo = new Repository(@"C:\Users\Ndiph\Documents\Git\WindowsPlatform\");
 			var branches = repo.Branches;
-			var bname = repo.Branches.FirstOrDefault(b => b.FriendlyName == "develop");
-			MessageBox.Show(bname.FriendlyName);
-			MessageBox.Show(bname != null ? bname.FriendlyName: "notInitalized");
+			var name = repo.Branches.FirstOrDefault(b => b.FriendlyName == "develop");
+			MessageBox.Show(name.FriendlyName);
+			MessageBox.Show(name != null ? $"You are on the branch + {name.FriendlyName}" : name.FriendlyName);
 
 			repo.Dispose();
 		}
+		private string GetCurrentRepositoryName()
+		{
+			string repoName = "NoName";
+			try
+			{
+				using (var repo = new Repository(@"C:\Users\Ndiph\Documents\Git\WindowsPlatform\"))
+				{
+					var repoPath = repo.Info.WorkingDirectory.TrimEnd(System.IO.Path.DirectorySeparatorChar);
+					repoName = System.IO.Path.GetFileName(repoPath);
+					lbl_response.Content = repoName;
+				}
+			}
+			catch (RepositoryNotFoundException)
+			{
+				MessageBox.Show("No repository found.");
+			}
+			return repoName;
+		}
+
 		private void OnFileClick(object s, RoutedEventArgs e)
 		{
-
+			lbl_response.Content = "File tab clicked.";
 		}
-		private void OnToolsClick(object s, RoutedEventArgs e)
+		private void OnOptionsClick(object s, RoutedEventArgs e)
 		{
-
+			lbl_response.Content = "Options tab clicked.";
 		}
 		private void OnHelpClick(object s, RoutedEventArgs e)
 		{
-
+			lbl_response.Content = "Help tab clicked.";
 		}
-
-		private void OnClickViewFiles(object sender, RoutedEventArgs e)
+		private void OnToolsClick(object s, RoutedEventArgs e)
 		{
-
-        }
-
+			lbl_response.Content = "Tools tab clicked.";
+		}
 		private void OnStageClick(object sender, RoutedEventArgs e)
 		{
-
+			lbl_response.Content = "Stage tab clicked.";
 		}
 	}
 }
